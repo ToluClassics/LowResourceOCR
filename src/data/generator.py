e0c4ddfd1103cfd18e030c0b85b6f2ff962b68b7
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2021-11-12 05:43:16 -0500

    initial commit

---
+++ src/data/generator.py
@@ -0,0 +1,121 @@
+"""
+Uses generator functions to supply train/test with data.
+Image renderings and text are created on the fly each time.
+"""
+
+from itertools import groupby
+from torch.utils.data import Dataset
+import torch
+
+import src.data.preprocess as pp
+#import preprocess as pp
+import os
+import numpy as np
+import unicodedata
+import torchvision.transforms as T
+from PIL import Image
+from numpy import asarray
+
+
+class DataGenerator(Dataset):
+    """Generator class with data streaming"""
+
+    def __init__(self, source, charset, max_text_length, transform):
+        self.tokenizer = Tokenizer(charset, max_text_length)
+        self.transform = transform
+
+        self.source = os.path.join(source, 'image')
+        self.images = os.listdir(self.source)
+        self.images = [image for image in self.images if image.endswith('.jpg')]
+
+        self.image_dataset = [asarray(Image.open(os.path.join(self.source, img))) for img in self.images]
+
+        with open(os.path.join(source, 'gt_test.txt'), "r") as f:
+            text = f.read()
+        text = text.split('\n')
+        text = [item.split() for item in text if len(item.strip()) > 1 ]
+        self.gt = {k[0]: ' '.join(k[1:]) for k in text}
+
+        self.size = len(self.images)
+
+    def __getitem__(self, i):
+        img = self.images[i]
+        img = os.path.join(self.source, img)
+        img = pp.preprocess(img, (1024, 128, 1))
+        # making image compatible with resnet
+        img = np.repeat(img[..., np.newaxis], 3, -1)
+        img = pp.normalization(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        y_train = self.tokenizer.encode(self.gt[self.images[i]])
+
+        # padding till max length
+        y_train = np.pad(y_train, (0, self.tokenizer.maxlen - len(y_train)))
+
+        gt = torch.Tensor(y_train)
+
+        return img, gt
+
+    def __len__(self):
+        return self.size
+
+
+class Tokenizer():
+    """Manager tokens functions and charset/dictionary properties"""
+
+    def __init__(self, chars, max_text_length=128):
+        self.PAD_TK, self.UNK_TK, self.SOS, self.EOS = "¶", "¤", "SOS", "EOS"
+        self.chars = [self.PAD_TK] + [self.UNK_TK] + [self.SOS] + [self.EOS] + list(chars)
+        self.PAD = self.chars.index(self.PAD_TK)
+        self.UNK = self.chars.index(self.UNK_TK)
+
+        self.vocab_size = len(self.chars)
+        self.maxlen = max_text_length
+
+    def encode(self, text):
+        """Encode text to vector"""
+
+        text = unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("ASCII")
+        text = " ".join(text.split())
+
+        groups = ["".join(group) for _, group in groupby(text)]
+        text = "".join([self.UNK_TK.join(list(x)) if len(x) > 1 else x for x in groups])
+        encoded = []
+
+        text = ['SOS'] + list(text) + ['EOS']
+        for item in text:
+            index = self.chars.index(item)
+            index = self.UNK if index == -1 else index
+            encoded.append(index)
+
+        return np.asarray(encoded)
+
+    def decode(self, text):
+        """Decode vector to text"""
+
+        decoded = "".join([self.chars[int(x)] for x in text if x > -1])
+        decoded = self.remove_tokens(decoded)
+        decoded = pp.text_standardize(decoded)
+
+        return decoded
+
+    def remove_tokens(self, text):
+        """Remove tokens (PAD) from text"""
+
+        return text.replace(self.PAD_TK, "").replace(self.UNK_TK, "")
+
+
+if __name__ == "__main__":
+    charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW\"XYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+    max_text_length = 100
+    split = "train"
+    transform = T.Compose([
+        T.ToTensor()])
+    dg = DataGenerator("raw_data/IAM", charset, max_text_length, transform)
+    train_loader = torch.utils.data.DataLoader(dg, batch_size=16, shuffle=False, num_workers=2)
+    for a ,b in train_loader:
+        print(a.shape)
+        print(b.shape)
+        break
---
+++ trainer.py
@@ -0,0 +1,73 @@
+from torch import nn
+import torch
+from torch.autograd import Variable
+import numpy as np
+import time
+
+device = "cuda:0" if torch.cuda.is_available() == True else "cpu"
+
+class LabelSmoothing(nn.Module):
+    "Implement label smoothing."
+
+    def __init__(self, size, padding_idx=0, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.padding_idx] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, Variable(true_dist, requires_grad=False))
+
+
+def train(model, criterion, optimiser, scheduler, dataloader, tokenizer):
+    model.train()
+    total_loss = 0
+    for batch, (imgs, labels_y,) in enumerate(dataloader):
+        imgs = imgs.to(device)
+        labels_y = labels_y.to(device)
+
+        optimiser.zero_grad()
+        output = model(imgs.float(), labels_y.long()[:, :-1])
+
+        norm = (labels_y != 0).sum()
+        loss = criterion(output.log_softmax(-1).contiguous().view(-1, tokenizer.vocab_size),
+                         labels_y[:, 1:].contiguous().view(-1).long()) / norm
+
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.2)
+        optimiser.step()
+        total_loss += loss.item() * norm
+
+    return total_loss / len(dataloader)
+
+
+def evaluate(model, criterion, dataloader, tokenizer):
+    model.eval()
+    epoch_loss = 0
+
+    with torch.no_grad():
+        for batch, (imgs, labels_y,) in enumerate(dataloader):
+            imgs = imgs.to(device)
+            labels_y = labels_y.to(device)
+
+            output = model(imgs.float(), labels_y.long()[:, :-1])
+
+            norm = (labels_y != 0).sum()
+            loss = criterion(output.log_softmax(-1).contiguous().view(-1, tokenizer.vocab_size),
+                             labels_y[:, 1:].contiguous().view(-1).long()) / norm
+
+            epoch_loss += loss.item() * norm
+
+    return epoch_loss / len(dataloader)
