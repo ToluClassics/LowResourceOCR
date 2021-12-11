e0ec7b356a8c920e59e4be6f5d07bd8bbcb4117e
Odunayo Ogundepo <odunayo@narval2.narval.calcul.quebec>
2021-12-11 16:53:25 -0500

    changes

--- sbatch.sh
+++ sbatch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=multiocr
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100:4
+#SBATCH --gres=gpu:a100:2
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=360GB
+#SBATCH --mem=240GB
 #SBATCH --time=72:00:00
 #SBATCH --account=def-kshook
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 
-#SBATCH -o ./output_igbo.log
+#SBATCH -o ./output_yor.log
 
 
 
@@ -18,4 +18,4 @@ source $ENVDIR/bin/activate
 
 python3 -c "import torch; torch.cuda.empty_cache()"
 
-nohup python3 main.py --lang igbo
+nohup python3 main.py --lang yor
